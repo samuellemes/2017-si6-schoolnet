a51482a49a96fef934135aae6a563b9f32edcd9f
Samuel Lemes <samuellemes25@gmail.com>
2017-11-16 21:40:38 -0200

    modify server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000
 
 
 // express setup
-const bodyParser = require(body-parser)
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extend: true }))
 app.use(express.static('public'))
 
@@ -14,8 +14,12 @@ app.set('views', path.join(__dirname, 'public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (request, response) => {
+   const data = {
+       name: 'Samuel',
+       email: 'samuel@mail'
+    }
     response.render('index')
-},
+}),
 
 app.listen(PORT, () => { 
     console.log('Server is running on port ' + PORT)
